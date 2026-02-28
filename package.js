bcd71edc78922d950a689b05e0b33a159ba4201f
Ai technologie <144942698+botwa7@users.noreply.github.com>
2026-02-28 13:50:31 -0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,30 @@
+{
+  "name": "EdwaTECH-md",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot Baileys Gifted Edition",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": ["whatsapp", "bot", "baileys"],
+  "author": "EdwaTECH",
+  "license": "MIT",
+  "dependencies": {
+    "gifted-baileys": "^1.0.0",
+    "pino": "^8.11.0",
+    "express": "^4.18.2",
+    "socket.io": "^4.6.1",
+    "qrcode-terminal": "^0.12.0",
+    "qrcode": "^1.5.3",
+    "axios": "^1.6.0",
+    "fs-extra": "^11.1.1",
+    "file-type": "^16.5.3",
+    "human-readable": "^0.2.1",
+    "moment-timezone": "^0.5.43",
+    "node-fetch": "^2.6.1"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+      }
